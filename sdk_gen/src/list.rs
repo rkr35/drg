9903b20046c8c29ec4782581fd199f3e70383297
rkr35 <rkr35@dragons.drexel.edu>
2021-06-16 20:17:59 -0400

    Relax `List` type restriction to no longer require `T: Copy + Default`

--- sdk_gen/src/list.rs
+++ sdk_gen/src/list.rs
@@ -1,32 +1,63 @@
+use core::mem::MaybeUninit;
+use core::ptr;
+use core::slice;
+
 #[derive(macros::NoPanicErrorDebug)]
 pub enum Error {
     CapacityReached,
 }
 
-pub struct List<T: Copy + Default, const N: usize> {
-    data: [T; N],
+pub struct List<T, const N: usize> {
+    data: [MaybeUninit<T>; N],
     len: usize,
 }
 
-impl<T: Copy + Default, const N: usize> List<T, N> {
+impl<T, const N: usize> List<T, N> {
+    const UNINITIALIZED_VALUE: MaybeUninit<T> = MaybeUninit::uninit();
+
     pub fn new() -> Self {
         Self { 
-            data: [T::default(); N],
+            data: [Self::UNINITIALIZED_VALUE; N],
             len: 0,
         }
     }
 
     pub fn iter(&self) -> impl Iterator<Item=&T> {
-        self.data.iter().take(self.len)
+        self.as_slice().iter()
     }
 
-    pub fn add(&mut self, value: T) -> Result<(), Error> {
+    pub fn push(&mut self, value: T) -> Result<(), Error> {
         if self.len < self.data.len() {
-            self.data[self.len] = value;
+            // Safe to use direct assignment since dropping a MaybeUninit<T> is a no-op.
+            self.data[self.len] = MaybeUninit::new(value);
             self.len += 1;
             Ok(())
         } else {
             Err(Error::CapacityReached)
         }
+    }
+
+    fn as_slice(&self) -> &[T] {
+        unsafe {
+            // SAFETY: We ensure that &self.data[..self.len] contains initialized values.
+            slice::from_raw_parts(self.data.as_ptr() as *const T, self.len)
+        }
+    }
+
+    fn as_mut_slice(&mut self) -> &mut [T] {
+        unsafe {
+            // SAFETY: We ensure that &self.data[..self.len] contains initialized values.
+            slice::from_raw_parts_mut(self.data.as_mut_ptr() as *mut T, self.len)
+        }
+    }
+}
+
+impl<T, const NUM_OUTERS: usize> Drop for List<T, NUM_OUTERS> {
+    fn drop(&mut self) {
+        // Drop initialized `MaybeUninit<T>`s.
+
+        unsafe {
+            ptr::drop_in_place(self.as_mut_slice());
+        } 
     }
 }
