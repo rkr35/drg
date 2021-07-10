b8cb8d8989f1e859d751f163d2bc58904a78389e
rkr35 <rkr35@dragons.drexel.edu>
2021-07-10 18:00:21 -0400

    Refactor `FullName` to use `ReverseSplitIterator`

--- sdk_gen/src/game/full_name.rs
+++ sdk_gen/src/game/full_name.rs
@@ -1,4 +1,5 @@
 use crate::list::{self, List};
+use crate::split::ReverseSplitIterator;
 use core::convert::TryFrom;
 
 #[derive(macros::NoPanicErrorDebug)]
@@ -23,44 +24,6 @@ fn split_class_and_outers(name: &str) -> Result<(&[u8], &[u8]), Error> {
     Ok((&name[..space], &name[space + 1..]))
 }
 
-struct OutersIterator<'name> {
-    outers: &'name [u8],
-}
-
-impl<'name> Iterator for OutersIterator<'name> {
-    type Item = &'name [u8];
-
-    fn next(&mut self) -> Option<Self::Item> {
-        #[allow(clippy::int_plus_one)]
-        // Applying this lint yields `i < self.outers.len()`, which doesn't elide the panic branch.
-        if let Some(split) = self
-            .outers
-            .iter()
-            .rposition(|c| *c == b'.')
-            .filter(|i| i + 1 <= self.outers.len())
-        {
-            // Return everything after the delimiter.
-            let ret = &self.outers[split + 1..];
-
-            // Shrink the input up to and excluding the delimiter.
-            self.outers = &self.outers[..split];
-
-            Some(ret)
-        } else if self.outers.is_empty() {
-            // We've exhausted the input, and there's nothing else to return.
-            None
-        } else {
-            // Return the remaining piece.
-            let ret = self.outers;
-
-            // Signal that we exhausted the input.
-            self.outers = &[];
-
-            Some(ret)
-        }
-    }
-}
-
 impl<'name, const NUM_OUTERS: usize> TryFrom<&'name str> for FullName<'name, NUM_OUTERS> {
     type Error = Error;
 
@@ -69,7 +32,7 @@ impl<'name, const NUM_OUTERS: usize> TryFrom<&'name str> for FullName<'name, NUM
 
         // Reverse split because outers are organized inside-out within an
         // object.
-        let mut outers = OutersIterator { outers };
+        let mut outers = ReverseSplitIterator::new(outers, b'.');
 
         // The first "outer" in the input name is actually the object name.
         let name = outers.next().ok_or(Error::NoName)?;
