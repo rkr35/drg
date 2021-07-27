61ca2876a6f5442d7c2856f8a489937cef0a0cc7
rkr35 <rkr35@dragons.drexel.edu>
2021-07-27 19:33:08 -0400

    Introduce `common` module to house common structures
    Migrate `TArray` and `FString` as inaugural test subjects.
    
    We'll want to move common game structures and abstractions into this
    module, so the generator, SDK, and hook can all reuse common definitions and implementations.

---
+++ common/src/lib.rs
@@ -0,0 +1,24 @@
+#![no_std]
+use core::ptr;
+use core::slice;
+
+#[repr(C)]
+pub struct TArray<T> {
+    data: *const T,
+    len: i32,
+    capacity: i32,
+}
+
+impl<T> TArray<T> {
+    pub fn as_slice(&self) -> &[T] {
+        unsafe {
+            if self.data.is_null() || self.len == 0 {
+                slice::from_raw_parts(ptr::NonNull::dangling().as_ptr(), 0)
+            } else {
+                slice::from_raw_parts(self.data, self.len as usize)
+            }
+        }
+    }
+}
+
+pub type FString = TArray<u16>;
--- sdk_gen/src/game.rs
+++ sdk_gen/src/game.rs
@@ -9,8 +9,9 @@ use core::fmt::{self, Display, Formatter};
 use core::mem;
 use core::ops::BitOr;
 use core::ptr;
-use core::slice;
 use core::str;
+
+use common::{FString, TArray};
 
 pub static mut NamePoolData: *const FNamePool = ptr::null();
 pub static mut GUObjectArray: *const FUObjectArray = ptr::null();
@@ -795,27 +796,6 @@ pub struct UEnum {
 }
 
 impl_deref! { UEnum as UField }
-
-#[repr(C)]
-pub struct TArray<T> {
-    data: *const T,
-    len: i32,
-    capacity: i32,
-}
-
-impl<T> TArray<T> {
-    pub fn as_slice(&self) -> &[T] {
-        unsafe {
-            if self.data.is_null() || self.len == 0 {
-                slice::from_raw_parts(ptr::NonNull::dangling().as_ptr(), 0)
-            } else {
-                slice::from_raw_parts(self.data, self.len as usize)
-            }
-        }
-    }
-}
-
-pub type FString = TArray<u16>;
 
 #[repr(C)]
 pub struct TPair<K, V> {
