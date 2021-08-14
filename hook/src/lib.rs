9332b44e7c95073cace314dddbd5ecf785c95a80
rkr35 <rkr35@dragons.drexel.edu>
2021-08-14 18:31:17 -0400

    Set up for detouring `ProcessEvent`

--- hook/src/lib.rs
+++ hook/src/lib.rs
@@ -1,5 +1,15 @@
 #![no_std]
 
+// // https://docs.microsoft.com/en-us/cpp/c-runtime-library/crt-library-features?view=msvc-160
+// #[link(name = "ucrt")]
+// extern {}
+
+#[link(name = "msvcrt")]
+extern "C" {}
+
+#[link(name = "vcruntime")]
+extern "C" {}
+
 use common::{self, win, UFunction, UObject};
 use core::ffi::c_void;
 
@@ -37,16 +47,21 @@ unsafe fn run() -> Result<(), Error> {
     let cave_size = code_cave.len();
 
     common::log!(
-        "Module starts at {} and is {} bytes.",
+        "Module starts at {} and is {} bytes.\n\
+        Largest code cave begins at {} and is {} bytes.\n\
+        my_process_event is at {}",
         module.start(),
-        module.size()
-    );
-    common::log!(
-        "Largest code cave begins at {} and is {} bytes.",
+        module.size(),
         code_cave.as_ptr() as usize,
-        cave_size
+        cave_size,
+        my_process_event as usize,
     );
 
     Ok(())
 }
+
 unsafe fn on_detach() {}
+
+unsafe extern "C" fn my_process_event(object: *mut UObject, function: *mut UFunction, parameters: *mut c_void) {
+    common::log!("my_process_event({}, {}, {})", *object, *function, parameters as usize);
+}
