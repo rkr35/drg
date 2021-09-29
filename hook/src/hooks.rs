7d3be4f1642f19d9e19c06cb66f2cd17cc0f4da0
rkr35 <rkr35@dragons.drexel.edu>
2021-09-28 22:10:24 -0400

    Add first attempt of a more generalized detour
    I want to make it easier to detour functions. Previously, I wrote out the ProcessEvent detour by hand, but a lot of that logic is constant between detours, so I extracted those pieces into `Detour` and `CodeCave` and rewrote the ProcessEvent detour in terms of those structures.

--- hook/src/hooks.rs
+++ hook/src/hooks.rs
@@ -9,7 +9,7 @@ use patch::Patch;
 
 mod user;
 
-static mut PROCESS_EVENT: *const c_void = ptr::null();
+static mut PROCESS_EVENT: *mut c_void = ptr::null_mut();
 static mut DRAW_TRANSITION: *const c_void = ptr::null();
 
 #[derive(macros::NoPanicErrorDebug)]
@@ -17,6 +17,106 @@ pub enum Error {
     FindProcessEvent,
     NoCodeCave,
     CaveIsTooSmall(usize, usize),
+}
+
+pub struct CodeCave<const JMP_LEN: usize> {
+    _jmp_to_hook: Patch<[u8; 12]>,
+    _original_bytes: Patch<[u8; JMP_LEN]>,
+    _jmp_to_original: Patch<[u8; 5]>,
+}
+
+impl<const JMP_LEN: usize> CodeCave<JMP_LEN> {
+    pub unsafe fn new(code_cave: &mut [u8], original: *const u8, hook: *const c_void) -> Result<CodeCave<JMP_LEN>, Error> {
+        let mut jmp_to_hook = [0x48, 0xB8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xE0];
+        (&mut jmp_to_hook[2..10]).copy_from_slice(&(hook as usize).to_le_bytes());
+
+        let mut original_bytes = [0; JMP_LEN];
+        original_bytes.copy_from_slice(slice::from_raw_parts(original, JMP_LEN));
+
+        let mut jmp_to_original = [0xE9, 0x00, 0x00, 0x00, 0x00];
+
+        let total_patch_len = jmp_to_hook.len() + original_bytes.len() + jmp_to_original.len();
+
+        if code_cave.len() < total_patch_len {
+            return Err(Error::CaveIsTooSmall(code_cave.len(), total_patch_len));
+        }
+
+        (&mut jmp_to_original[1..]).copy_from_slice({
+            let destination = original as usize + JMP_LEN;
+            let source = code_cave.as_ptr() as usize + total_patch_len;
+            let relative_distance = destination.wrapping_sub(source) as u32;
+            &relative_distance.to_le_bytes()
+        });
+
+        let code_cave = code_cave.as_mut_ptr();
+
+        Ok(CodeCave {
+            _jmp_to_hook: Patch::new(code_cave.cast(), jmp_to_hook),
+            _original_bytes: Patch::new(code_cave.add(jmp_to_hook.len()).cast(), original_bytes),
+            _jmp_to_original: Patch::new(code_cave.add(jmp_to_hook.len() + original_bytes.len()).cast(), jmp_to_original),
+        })
+    }
+}
+
+pub struct Detour<const JMP_LEN: usize> {
+    jmp: ManuallyDrop<Patch<[u8; JMP_LEN]>>,
+    code_cave: ManuallyDrop<CodeCave<JMP_LEN>>,
+}
+
+impl<const JMP_LEN: usize> Detour<JMP_LEN> {
+    pub unsafe fn new(module: &win::Module, original: *mut *mut c_void, hook: *const c_void) -> Result<Detour<JMP_LEN>, Error> {
+        let code_cave = module.find_code_cave().ok_or(Error::NoCodeCave)?;
+
+        let code_cave_patch = ManuallyDrop::new(CodeCave::new(
+            code_cave, *original.cast(), hook
+        )?);
+
+        // There's something to be desired about this variable name...
+        let original_original = *original;
+
+        // TODO(unhook): Restore to original address.
+        *original = code_cave.as_mut_ptr().add(12).cast();
+
+        let jmp = ManuallyDrop::new(Patch::new(
+            original_original.cast(),
+            Self::create_jmp_patch(code_cave, original_original),
+        ));
+
+        Ok(Detour {
+            jmp,
+            code_cave: code_cave_patch,
+        })
+    }
+
+    unsafe fn create_jmp_patch(code_cave: &[u8], original: *const c_void) -> [u8; JMP_LEN] {
+        let mut patch = [0; JMP_LEN];
+
+        // jmp code_cave
+        patch[0] = 0xE9;
+
+        (&mut patch[1..5]).copy_from_slice({
+            let destination = code_cave.as_ptr() as usize;
+            let source = original as usize + 5;
+            let relative_distance = destination.wrapping_sub(source) as u32;
+            &relative_distance.to_le_bytes()
+        });
+
+        // no-ops to patch cleaved instructions
+        (&mut patch[5..]).fill(0x90);
+
+        patch
+    }
+}
+
+impl<const JMP_LEN: usize> Drop for Detour<JMP_LEN> {
+    fn drop(&mut self) {
+        unsafe {
+            ManuallyDrop::drop(&mut self.jmp);
+            // Before we destroy the code cave, give the CPU time to exit the cave.
+            win::Sleep(100);
+            ManuallyDrop::drop(&mut self.code_cave);
+        }
+    }
 }
 
 pub struct Hooks {
@@ -34,139 +134,18 @@ impl Hooks {
 }
 
 struct ProcessEventHook {
-    jmp: ManuallyDrop<Patch<[u8; 6]>>,
-    code_cave: ManuallyDrop<Patch<[u8; 23]>>,
-}
-
-impl Drop for ProcessEventHook {
-    fn drop(&mut self) {
-        unsafe {
-            ManuallyDrop::drop(&mut self.jmp);
-            // Before we destroy the code cave, give the CPU time to exit the cave.
-            win::Sleep(100);
-            ManuallyDrop::drop(&mut self.code_cave);
-        }
-    }
+    _detour: Detour<6>,
 }
 
 impl ProcessEventHook {
     pub unsafe fn new(module: &win::Module) -> Result<ProcessEventHook, Error> {
-        let process_event: *mut u8 = module
-            .find_mut(&[
-                Some(0x40),
-                Some(0x55),
-                Some(0x56),
-                Some(0x57),
-                Some(0x41),
-                Some(0x54),
-                Some(0x41),
-                Some(0x55),
-                Some(0x41),
-                Some(0x56),
-                Some(0x41),
-                Some(0x57),
-                Some(0x48),
-                Some(0x81),
-                Some(0xEC),
-                Some(0xF0),
-                Some(0x00),
-                Some(0x00),
-                Some(0x00),
-            ])
-            .ok_or(Error::FindProcessEvent)?;
+        const PATTERN: [Option<u8>; 19] = [Some(0x40), Some(0x55), Some(0x56), Some(0x57), Some(0x41), Some(0x54), Some(0x41), Some(0x55), Some(0x41), Some(0x56), Some(0x41), Some(0x57), Some(0x48), Some(0x81), Some(0xEC), Some(0xF0), Some(0x00), Some(0x00), Some(0x00)];
 
-        let code_cave = module.find_code_cave().ok_or(Error::NoCodeCave)?;
-        let cave_size = code_cave.len();
-
-        common::log!(
-            "Module starts at {} and is {} bytes.\n\
-            Largest code cave begins at {} and is {} bytes.\n\
-            process_event is at {}\n\
-            my_process_event is at {}",
-            module.start(),
-            module.size(),
-            code_cave.as_ptr() as usize,
-            cave_size,
-            process_event as usize,
-            user::my_process_event as usize,
-        );
-
-        let code_cave_patch = ManuallyDrop::new(Patch::new(
-            code_cave.as_mut_ptr().cast(),
-            Self::create_code_cave_patch(code_cave, process_event)?,
-        ));
-
-        let jmp = ManuallyDrop::new(Patch::new(
-            process_event.cast(),
-            Self::create_jmp_patch(code_cave, process_event),
-        ));
+        PROCESS_EVENT = module.find_mut(&PATTERN).ok_or(Error::FindProcessEvent)?;
 
         Ok(ProcessEventHook {
-            jmp,
-            code_cave: code_cave_patch,
+            _detour: Detour::new(module, &mut PROCESS_EVENT, user::my_process_event as *const c_void)?,
         })
-    }
-
-    unsafe fn create_jmp_patch(code_cave: &[u8], process_event: *const u8) -> [u8; 6] {
-        let mut patch = [
-            // jmp code_cave (need to fill in)
-            0xE9, 0x00, 0x00, 0x00, 0x00,
-            // nop (otherwise we would cut a two byte instruction in half)
-            0x90,
-        ];
-
-        let destination = code_cave.as_ptr() as usize;
-        let source = process_event as usize + 5;
-        let relative_distance = destination.wrapping_sub(source) as u32;
-        (&mut patch[1..=mem::size_of::<u32>()]).copy_from_slice(&relative_distance.to_le_bytes());
-
-        patch
-    }
-
-    unsafe fn create_code_cave_patch(
-        code_cave: &[u8],
-        process_event: *const u8,
-    ) -> Result<[u8; 23], Error> {
-        #[rustfmt::skip]
-        let mut patch = [
-            // mov rax, user::my_process_event (need to fill in)
-            0x48, 0xB8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-
-            // jmp rax
-            0xFF, 0xE0,
-
-            // first six bytes of ProcessEvent that we overwrote with the jmp to codecave (need to fill in)
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-
-            // jmp ProcessEvent+6 (need to fill in)
-            0xE9, 0x00, 0x00, 0x00, 0x00,
-        ];
-
-        if code_cave.len() < patch.len() {
-            return Err(Error::CaveIsTooSmall(code_cave.len(), patch.len()));
-        }
-
-        // mov rax, my_process_event
-        (&mut patch[2..2 + mem::size_of::<usize>()])
-            .copy_from_slice(&(user::my_process_event as usize).to_le_bytes());
-
-        // first six bytes of ProcessEvent
-        let first_six_process_event_bytes = slice::from_raw_parts(process_event, 6);
-        (&mut patch[12..12 + first_six_process_event_bytes.len()])
-            .copy_from_slice(first_six_process_event_bytes);
-
-        PROCESS_EVENT = code_cave.as_ptr().add(12).cast();
-
-        // jmp ProcessEvent+6
-        let patch_len = patch.len();
-        (&mut patch[19..19 + mem::size_of::<u32>()]).copy_from_slice({
-            let destination = process_event as usize + first_six_process_event_bytes.len();
-            let source = code_cave.as_ptr() as usize + patch_len;
-            let relative_distance = destination.wrapping_sub(source) as u32;
-            &relative_distance.to_le_bytes()
-        });
-
-        Ok(patch)
     }
 }
 
