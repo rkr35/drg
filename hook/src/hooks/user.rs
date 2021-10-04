f5a062e1e909773cc2121f436ffcca7804eefe48
rkr35 <rkr35@dragons.drexel.edu>
2021-10-03 20:52:14 -0400

    Remove canvas test

--- hook/src/hooks/user.rs
+++ hook/src/hooks/user.rs
@@ -44,29 +44,6 @@ pub unsafe extern "C" fn my_draw_transition(
     game_viewport_client: *mut GameViewportClient,
     canvas: *mut Canvas,
 ) {
-    const WIDTH: f32 = 1024.0;
-    const HEIGHT: f32 = 768.0;
-
-    const SIZE: f32 = 100.0;
-
-    let position = Vector2D {
-        X: WIDTH / 2.0 - SIZE / 2.0,
-        Y: HEIGHT / 2.0 - SIZE / 2.0,
-    };
-
-    let size = Vector2D { X: SIZE, Y: SIZE };
-
-    let thickness = 1.0;
-
-    let color = LinearColor {
-        R: 0.0,
-        G: 1.0,
-        B: 1.0,
-        A: 1.0,
-    };
-
-    Canvas::K2_DrawBox(canvas, position, size, thickness, color);
-
     type DrawTransition = unsafe extern "C" fn(*mut GameViewportClient, *mut Canvas);
     let original = mem::transmute::<*const c_void, DrawTransition>(crate::hooks::DRAW_TRANSITION);
     original(game_viewport_client, canvas);
