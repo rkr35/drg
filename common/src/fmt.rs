8e3fb06e516a8a74ff0d378e929ed51119eda9d6
rkr35 <rkr35@dragons.drexel.edu>
2021-10-31 21:40:21 -0400

    #12: Add todos for hex formatting

--- common/src/fmt.rs
+++ common/src/fmt.rs
@@ -30,7 +30,7 @@ pub struct Hex<T>(pub T);
 impl<T: Hexable> Display for Hex<T> {
     fn fmt(&self, f: &mut Formatter) -> Result<(), fmt::Error> {
         const PREFIX_LEN: usize = 2;
-        const MAX_HEX_DIGITS: usize = 16;
+        const MAX_HEX_DIGITS: usize = 16; // TODO: i128/u128
         const MAX_FORMATTED_LEN: usize = PREFIX_LEN + MAX_HEX_DIGITS;
 
         let mut buffer = [0; MAX_FORMATTED_LEN];
@@ -58,6 +58,7 @@ impl<T: Hexable> Display for Hex<T> {
         buffer[cursor + 1] = b'x';
 
         // SAFETY: We fill `buffer` with only characters in [0-9a-fx], which are ASCII.
+        // TODO(safety): negative numbers
         f.write_str(unsafe { str::from_utf8_unchecked(&buffer[cursor..]) })
     }
 }
