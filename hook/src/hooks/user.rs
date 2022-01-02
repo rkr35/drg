4eee42b8d94e50906e0f574fae9e6ff69dd764b9
rkr35 <rkr35@dragons.drexel.edu>
2022-01-02 11:18:23 -0500

    Doesn't work, so comment out 7ce72903b92fb17eda8c89d7af00c4dabf3b7760 for now

--- hook/src/hooks/user.rs
+++ hook/src/hooks/user.rs
@@ -128,41 +128,41 @@ pub unsafe extern "C" fn my_route_end_play(actor: *mut Actor, end_play_reason: u
     original(actor, end_play_reason);
 }
 
-#[repr(C)]
-pub struct Id {
-    vtable: usize,
-    this: *mut Id,
-    magic: usize,
-    value: u64,
-}
+// #[repr(C)]
+// pub struct Id {
+//     vtable: usize,
+//     this: *mut Id,
+//     magic: usize,
+//     value: u64,
+// }
 
-#[repr(C)]
-pub struct IdWrapper {
-    vtable: usize,
-    id: *mut Id,
-    magic: usize,
-    pad: [u8; 16],
-}
+// #[repr(C)]
+// pub struct IdWrapper {
+//     vtable: usize,
+//     id: *mut Id,
+//     magic: usize,
+//     pad: [u8; 16],
+// }
 
-pub unsafe extern "C" fn my_get_preferred_unique_net_id(local_player: *mut LocalPlayer, out_id: *mut IdWrapper) -> *mut IdWrapper {
-    type GetPreferredUniqueNetId = unsafe extern "C" fn (*mut LocalPlayer, *mut IdWrapper) -> *mut IdWrapper;
-    let original = mem::transmute::<*const c_void, GetPreferredUniqueNetId>(crate::GET_PREFERRED_UNIQUE_NET_ID);
-    original(local_player, out_id);
+// pub unsafe extern "C" fn my_get_preferred_unique_net_id(local_player: *mut LocalPlayer, out_id: *mut IdWrapper) -> *mut IdWrapper {
+//     type GetPreferredUniqueNetId = unsafe extern "C" fn (*mut LocalPlayer, *mut IdWrapper) -> *mut IdWrapper;
+//     let original = mem::transmute::<*const c_void, GetPreferredUniqueNetId>(crate::GET_PREFERRED_UNIQUE_NET_ID);
+//     original(local_player, out_id);
 
-    let old_id = (*(*out_id).id).value;
+//     let old_id = (*(*out_id).id).value;
 
-    let new_id = {
-        const ID_WITHOUT_ACCOUNT: u64 = 76561197960265728;
-        let random_account = random::u32();
-        ID_WITHOUT_ACCOUNT | u64::from(random_account)
-    };
+//     let new_id = {
+//         const ID_WITHOUT_ACCOUNT: u64 = 76561197960265728;
+//         let random_account = random::u32();
+//         ID_WITHOUT_ACCOUNT | u64::from(random_account)
+//     };
 
-    (*(*out_id).id).value = new_id;
+//     (*(*out_id).id).value = new_id;
 
-    common::log!("ID: {} -> {}", old_id, new_id);
+//     common::log!("ID: {} -> {}", old_id, new_id);
 
-    out_id
-}
+//     out_id
+// }
 
 #[allow(dead_code)]
 unsafe fn print_if_unseen(object: *mut UObject, function: *mut UFunction) {
